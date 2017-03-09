1faeb4716e489c12df5ce60f0df0ef98394edbad
sbs205c5 <brtruitt1@gmail.com>
2017-03-09 11:39:11 -0700

    cleaned out repo

--- README.md
+++ README.md
@@ -1,6 +1,54 @@
 # Analysis of Multifamily Mortgage Default Probabilities
 
-## Project Overview:
-The focus of this analysis is to approximate the probability of default for multifamily mortgages. Machine learning algorithms will be applied to data provided publicly by the Federal Home Loan Mortgage Corporation (Freddie Mac). This data is for existing multifamily loans. The resulting probabilities are unique to each loan. You can view the everything from the initial exploratory data analysis to the final results at (AWS link provided).
+## Project Introduction
+The focus of this analysis is to approximate the probability of default for multifamily mortgages. I have worked in the area of multifamily lending for about 10 years. I have done everything from underwrite to originate to the sale of securities to large investment banks that would re-securitize the loans for consumers such as large institutional investors and hedge funds.
 
-**Data Source: ** [Freddie Mac Investor Access](https://msia.ficonsulting.com/)
+<div style="float: right; width: 40%;">
+    ![1600 Glenarm Place](/img/thumbs_1600-Glenarm-002.jpg)
+</div>
+
+The loan data that I will be using for this analysis represents the performance of Freddie Mac multifamily loans. Freddie Mac is the nickname used for the Federal Home Loan Mortgage Corporation (FHLMC). Freddie Mac works in partnership with a network of nationwide lenders to provide loans for both single-family and multifamily mortgages. A single-family property is defined as 1-4 units. A single-family mortgage is what you think of if you were to get a mortgage on your home. You may already have a Freddie Mac mortgage and not know it. A multifamily mortgage is what is provided for an apartment building or complex. A multifamily property is 5 units or more. An example of a multifamily property is 1600 Glenarm Place in Denver, CO. The focus of this analysis will be only multifamily mortgages.
+
+## Freddie Mac
+
+### U.S. Government Implicit Guarantee
+
+<div style="float: right; width: 60%;">
+    ![Freddie Fed Bailout](/img/investor-presentation.pdf.jpg)
+</div>
+
+During the financial crisis in 2008, Freddie Mac received a bailout from the U.S. Government due to defaulting mortgages in their portfolio. Freddie Mac was given a loan for $71.3 Billion. Since this time, Freddie Mac has repaid $105.9 Billion in dividends. When the U.S. Government bailed-out Freddie Mac, the statement was made that Freddie Mac was "too big to fail" and had the implicit guarantee of the U.S. Government. Investors now began buying Freddie Mac mortgage securities as if they had no default risk. This reduced the interest rate required by investors to less than what would be required if the loan were made by a normal bank.
+
+As of March 7th, 2017; if you look at the current interest rate required for a loan by Freddie Mac in comparison to a conventional / bank loan for a mortgage with a payment based on 30 years of amortization, term of 10 years, loan-to-value ratio of 80%, and a mortgage amount of $10,000,000, the rates would be:
+
+* Freddie Mac Loan: 4.450%
+* Conventional Loan: 4.711%
+
+This does not look like a significant difference; however, let's assume you were the one investing the $10,000,000 to provide this loan and you did it based on Freddie Mac's default free status. Now the federal government announces that they will no longer provide their implicit guarantee. Your $10,000,000 investment now drops to a value of $9,809,682 a difference of $190,318 (or 1.90%). This is approximately the value associated with the default risk priced into the conventional loan. This is the value associated with the U.S. Government's implicit guarantee.
+
+### Why Freddie Mac
+
+<div style="float: right; width: 50%;">
+    ![Freddie Fed Big Payment](/img/freddie-mac-to-pay-treasury-4-5-billion-after-reporting-profit.jpg)
+</div>
+
+The political climate and public opinion has shifted. Freddie Mac appears to be standing on their own two feet and the implicit guarantee does not seem as necessary, in particular, now that Freddie Mac has paid back their loan. It is quite possible that the U.S. Government's implicit guarantee could be removed.
+
+If the U.S. Government's implicit guarantee were to be removed it could significantly effect the value of Freddie Mac issued securities. This would be good for some and bad for others, but getting ahead of this by having a good understanding of the current default probabilities within the Freddie Mac loan portfolio and how to forecast the values that the securities would likely correct to is key in creating and profiting from the opportunities that will surely arise.
+
+## Consumers of This Model
+
+### Freddie Mac Security Investors
+Two of the driving factors for investors to invest in Freddie Mac (and other GSE) securities are their ability to be leveraged, such as by hedge funds, and their ability to meet the Dodd-Frank Act requirement that banks and insurance companies hold higher percentages of risk free assets on their balance sheets.
+
+The Dodd-Frank Act requirement is by far the largest domestic driver for the purchase of Freddie Mac securities. If a bank wants to increase their lending, they need to increase the amount of risk free assets on their balance sheets. If an insurance company wants to increase their liabilities, by issuing new policies, they need to hold more risk free assets. This aspect of the Dodd-Frank act is hendering the profitability of banks and insurance companies and they want it gone. They have been steadily lobbying for a change to this portion of the Dodd-Frank act and it may just be around the corner.
+
+Hedge funds create leverage in their portfolios by leveraging their purchases. Leveraging is just another term for making purchases using a loan from somebody else. The purchases of risk free assets can be leveraged to a higher ratio of around 1-to-4 than purchases of assets that have a higher risk of default, such as, Commercial Mortgage Backed Security (CMBS) assets which may be leverage to around 1-to-3. If you look back at our Freddie Mac interest rate of 4.450%. A hedge fund may pay around 2.5% to borrow money, so after leverage they are making a return of just over 10% for the Freddie Mac loan investment and just under 8% for the conventional loan investment.
+
+There are many likely to be affected by a removal of the U.S. Government's implicit guarantee, but a few that should be sure to pay particular attention to my results are insurance companies, banks, and hedge funds.
+
+**Link to Presentation Slides:** [Default Risk In Multifamily Mortgages](https://docs.google.com/presentation/d/1AvFxeSGNUIpF76LP149ydiUKDZK0Xt6j5LOWwd6UNew/pub?start=true&loop=true&delayms=3000)
+
+## Analysis
+
+Based on evaluation of loans foreclosed in the Freddie Mac portfolio, the average percentage of outstanding balance that is recovered following disposition is 0.6930486709.
---
+++ mf_mrtg_data_prep.py
@@ -0,0 +1,271 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+from datetime import datetime, timedelta
+from calendar import monthrange
+import csv
+from pdb import set_trace
+
+### Read in data and cleanup column headings
+def read_data_pandas(filename, sep=','):
+    df = pd.read_csv(filename, sep=sep)
+    cols = df.columns.tolist()
+    cols = [col.lower().replace(' ', '_') for col in cols]
+    df.columns = cols
+    return df
+
+def read_data_csv(filename):
+    with open(filename, 'rb') as csvfile:
+        filereader = csv.reader(csvfile, dialect='excel')
+        arr_list = []
+        for i, row in enumerate(filereader):
+             if i == 0: columns = row
+             else: arr_list.append(np.array(row))
+    np_data = np.array(arr_list)
+    df = pd.DataFrame(data=np_data[:,:len(columns)], columns=columns)
+    cols = df.columns.tolist()
+    cols = [col.lower().replace(' ', '_') for col in cols]
+    df.columns = cols
+    return df
+
+### Process Columns
+def map_dfs(df_mflp, df_mspd, df_map):
+
+    prob_loan_ids = df_mspd['loan_id'][(df_mspd['no_time_dlqlife']>0) | (df_mspd['date_added_to_servicer_watchlist_']!="-")].tolist()
+
+    map_cols, mflp_cols, mspd_cols = df_map.columns.tolist()
+
+    df_mflp = df_mflp[df_map[mflp_cols]]
+
+    df_mspd = df_mspd[df_map[mspd_cols]]
+
+    # Map df_mspd dlq_status_text column to corresponding values in df_MFLP
+    # 100 = Current or less than 60 day delinquent
+    # 200 = 60 or more days delinquent
+    # 300 = Foreclosure
+    # 350 = Problem Loans
+    # 450 = Real estate owned
+    # 500 = Closed
+    dlq_status_text_map = {
+    'Current':100,
+    '90+':200,
+    '< 30 ':350,
+    'Grace':100,
+    'Perf Balloon':350,
+    '-':100, '60-89':200}
+
+    dlq_status_text_list = df_mspd['dlq_status_text'].tolist()
+    df_mspd['dlq_status_text'] = [dlq_status_text_map[x] for x in dlq_status_text_list]
+
+    # Map df_mspd note_rate to be 0.0xxx format instead of x.xx% format
+    df_mspd['note_rate'] = np.divide(df_mspd['note_rate'], 100.)
+
+    # Convert df_mflp quarter column to date
+    eoqs = []
+    this_year = datetime.today().year - 2000
+    for eoq in df_mflp['quarter'].tolist():
+        eoq_yr = 1900+int(int(eoq[1:3])<=this_year)*100+int(eoq[1:3])
+        eoq_mo = int(eoq[-1])*3
+        eoq_day = monthrange(eoq_yr, eoq_mo)[1]
+        eoqs.append(str(eoq_mo) + '/' + str(eoq_day) + '/' + str(eoq_yr))
+    df_mflp['quarter'] = pd.to_datetime(np.array(eoqs), errors='coerce')
+    # set_trace()
+    # Get dataframe of delinquency status
+    df_mflp_dlq = df_mflp.loc[:,['lnno', 'mrtg_status']]
+
+    # Filter df_mflp to only newest entries
+    max_qt_series = df_mflp.groupby('lnno', as_index=False)['quarter'].transform('max')
+    df_mflp['max_qtr'] = max_qt_series
+    df_mflp = df_mflp[df_mflp['quarter'] == df_mflp['max_qtr']]
+
+    # Assign mrtg status in df_mflp to highest level prior to 500
+    new_mrtg_status = []
+    for ln_id in df_mflp['lnno'].tolist():
+        dlq_status_arr = df_mflp_dlq[df_mflp_dlq['lnno'] == ln_id]['mrtg_status'].unique()
+        if len(dlq_status_arr) > 1:
+            dlq_status_arr = np.delete(dlq_status_arr, np.where(dlq_status_arr==500))
+        new_mrtg_status.append(dlq_status_arr.max())
+    df_mflp['mrtg_status'] = np.array(new_mrtg_status)
+
+    df_mflp.drop(['max_qtr'], inplace=True, axis=1)
+
+    # add engineered columns to df_mflp
+    df_mflp.columns = df_map[map_cols]
+    df_mflp['published_date'] = pd.to_datetime(df_mflp['published_date'], errors='coerce')
+    df_mflp['freddie_held'] = 1.0
+    df_mflp['principal_paydown'] = df_mflp['original_balance'] - df_mflp['current_balance']
+    # define label to be loans beyond 60 days late and any that have defaulted in the past
+    df_mflp['label'] = [float(x) for x in df_mflp['loan_status'].isin([200,300,350,450])]
+    df_mflp.drop(['loan_status'], inplace=True, axis=1)
+    df_mflp.to_csv(datadir + 'df_mflp_labeled' + '.csv')
+
+    # add engineered columns to df_mspd
+    df_mspd.columns = df_map[map_cols]
+    df_mspd['published_date'] = pd.to_datetime(df_mspd['published_date'], errors='coerce')
+    df_mspd['freddie_held'] = 0.0
+    df_mspd['principal_paydown'] = df_mspd['original_balance'] - df_mspd['current_balance']
+    # define label to be loans beyond 60 days late and any that have defaulted in the past
+    df_mspd['label'] = [float(x) for x in ((df_mspd['loan_status'].isin([200,300,350,450])) | (df_mspd['loan_id'].isin(prob_loan_ids)))]
+    df_mspd.drop(['loan_status'], inplace=True, axis=1)
+    df_mspd.to_csv(datadir + 'df_mspd_labeled' + '.csv')
+
+    df_comb = pd.concat([df_mflp, df_mspd])
+    df_comb.drop(['current_balance', 'principal_paydown'], inplace=True, axis=1)
+    df_comb.set_index(np.arange(df_comb.shape[0]), drop=True, inplace=True)
+    df_comb.to_csv(datadir + 'df_comb_labeled' + '.csv')
+
+    return df_mflp, df_mspd, df_comb
+
+### Plot histograms for data columns
+def plot_histograms(df, columns, name='test', plotdir='plots/'):
+    plots = len(columns)
+    rem_plots = 0
+    y_plots = int(np.sqrt(plots))+1
+    x_plots = plots/y_plots
+    rem_plots = plots - (y_plots*x_plots)
+    x_plots += 1 if rem_plots else 0
+    plt.figure(figsize=(y_plots*5,x_plots*5))
+    plt.suptitle(name)
+    for i, col in enumerate(columns):
+        plt.subplot(y_plots, x_plots, i+1)
+        column_list = [x for x in df[col]]
+        labels, values = zip(*Counter(column_list).items())
+        indexes = np.arange(len(labels))
+        if type(labels[0]) == str:
+            idx = np.argsort(values)
+        else:
+            idx = np.argsort(labels)
+        labels = [labels[x] for x in idx]
+        values = [values[x] for x in idx]
+        width = 1
+        plt.bar(indexes, values, width, alpha=.5)
+        plt.title(col)
+        plt.xticks(indexes + width * 0.5, labels, rotation=70)
+    plt.tight_layout()
+    top_s = 1.-1./(3.*y_plots)
+    plt.subplots_adjust(top=top_s)
+    plt.savefig(plotdir + name + '_hist.png')
+    plt.close()
+
+### print table for markdown file
+def print_df_md_table(df):
+    headings = [('Column Name',':---'), ('Type',':---:'), ('Non-null',':---:'), ('Unique',':---:'), ('Example',':---')]
+    # set_trace()
+    keys = [x[0] for x in headings]
+    values = [x[1] for x in headings]
+    print '| ', ' | '.join(keys), ' |'
+    print '| ', ' | '.join(values), ' |'
+    for i, col in enumerate(df.columns.tolist()):
+        row = str(df.dtypes[i:i+1]).split()[:2]
+        row.append(str(df[col].notnull().sum()))
+        row.append(str(df[col].nunique()))
+        row.append(str(df[col].iloc[0]))
+        print '| ', ' | '.join(row), ' |'
+
+def clean_mspd_data(df):
+    columns = df.columns.tolist()
+    for col in columns:
+        # set default variable default values
+        blank = ''
+        number, text = False, False
+        # determine type of fill value used in column and replace
+        fill_vals = ['-', ' ', '']
+        for val in fill_vals:
+            if val in df[col].unique().tolist():
+                blank = val
+                break
+        temp_col = df[col].replace(blank, '0')
+        # change column type based on column contents
+        try:
+            temp_col = temp_col.replace('[$,]','', regex=True)
+            temp_col = temp_col.astype(float)
+            if np.mod(temp_col,1).sum() == 0: # removed: temp_col.nunique() < 375 and
+                temp_col = temp_col.astype(int)
+            df[col] = temp_col
+        except:
+            temp_col = np.sort(df[col].iloc[:500])[::-1]
+            try:
+                # set_trace()
+                temp_date = datetime.datetime.strptime(temp_col[0], '%m/%d/%Y')
+                df[col] = pd.to_datetime(df[col], errors='coerce')
+            except:
+                df[col] = df[col].astype(str)
+    ltv_col = df['uw_ltv'].replace('[%]','', regex=True)
+    ltv_col = np.divide(ltv_col.astype(float),100.)
+    df['uw_ltv'] = ltv_col
+    special_svcr = [x.lower().replace(' ', '_') for x in df['special_servicer']]
+    df['special_servicer'] = np.array(special_svcr)
+    return df
+
+if __name__ == '__main__':
+    datadir = 'data/'
+
+    ### open data and import into pandas dataframes
+    mflp_file = 'mlpd_datamart_1q16.txt'
+    mspd_file = 'custom_rpt_all_properties_20170222.csv'
+    ## Open Multifamily Loan Performance Data
+    df_mflp = read_data_pandas(datadir + mflp_file, sep='|')
+    df_mflp['special_svcr'] = 'freddie_mac'
+    # Clean DataFrame data
+    # df_mflp = clean_df_data(df_mflp)
+    ## Open Multifamily Securitization Program Data
+    df_mspd = read_data_pandas(datadir + mspd_file)
+    # Clean DataFrame data
+    df_mspd = clean_mspd_data(df_mspd)
+    df_mspd_o = df_mspd.copy()
+    df_mspd_o.to_csv(datadir + 'df_mspd_o.csv')
+
+    ### map input tables to new output table for analysis
+    # New table | MFLP table | MSPD table
+    col_map_a = [
+    ['loan_id', 'lnno','loan_id'],
+    ['loan_status', 'mrtg_status', 'dlq_status_text'],
+    ['published_date', 'quarter', 'distributiondate'],
+    ['current_balance', 'amt_upb_endg', 'actual_balance'],
+    ['original_balance', 'amt_upb_pch', 'original_note_amount'],
+    ['property_state', 'code_st', 'state'],
+    ['orig_int_rate', 'rate_int', 'note_rate'],
+    ['orig_dscr', 'rate_dcr', 'uw_dscr_(ncf)amortizing'],
+    ['orig_ltv', 'rate_ltv', 'uw_ltv'],
+    ['special_servicer', 'special_svcr', 'special_servicer']]
+
+    df_map = pd.DataFrame(data=col_map_a, columns=['col_a', 'mflp_col', 'mspd_col'], index=np.arange(len(col_map_a)))
+
+    df_mflp, df_mspd, df_comb = map_dfs(df_mflp, df_mspd, df_map)
+
+    # add additional columns to df_mspd (securitized set)
+    df_mspd['orig_occ_rate'] = np.divide(df_mspd_o['original_occupancy__rate'], 100.)
+    df_mspd['most_rct_occ_rate'] = np.divide(df_mspd_o['most_recentphys_occup'], 100.)
+    df_mspd['occ_rate_delta'] = df_mspd['most_rct_occ_rate'] - df_mspd['orig_occ_rate']
+
+    df_mspd['orig_noi'] = df_mspd_o['noi_at_contribution']
+    df_mspd['most_rct_noi'] = df_mspd_o['most_recent_noi']
+    df_mspd['noi_delta'] = df_mspd['most_rct_noi'] - df_mspd['orig_noi']
+
+    df_mspd['most_rct_value'] = df_mspd_o['most_recent_value']
+    df_mspd['orig_value'] = df_mspd['original_balance'] / df_mspd['orig_ltv']
+    df_mspd['orig_value'][df_mspd['orig_ltv']==0] = df_mspd['most_rct_value']
+    df_mspd['value_delta'] = df_mspd['most_rct_value'] - df_mspd['orig_value']
+
+    df_mspd['most_rct_ltv'] = df_mspd['current_balance'] / df_mspd['most_rct_value']
+    df_mspd['ltv_delta'] = df_mspd['most_rct_ltv'] - df_mspd['orig_ltv']
+
+    df_mspd['most_rct_debt_serv'] = df_mspd_o['most_recent_debt_service_amount']
+    df_mspd['orig_debt_serv'] = df_mspd['orig_noi'] / df_mspd['orig_dscr']
+    df_mspd['debt_serv_delta'] = df_mspd['most_rct_debt_serv'] - df_mspd['orig_debt_serv']
+
+    df_mspd['most_rct_dscr'] = df_mspd_o['most_recentdscr_(ncf)']
+    df_mspd['dscr_delta'] = df_mspd['most_rct_dscr'] - df_mspd['orig_dscr']
+
+    df_mspd.to_csv(datadir + 'df_mspd_labeled_built_up.csv')
+
+
+
+
+    # plot_hists = str(raw_input('Would you like to plot a histogram for each column? [y/n]'))
+    #
+    # hist_columns = ['balance_range', 'dscr_range', 'dlq_status_text', 'fka_status_of_loan', 'group_id', 'loan_amortization_type', 'ltv_range', 'master_servicer', 'most_recentfinancial_indicator', 'most_recentphys_occup', 'no_time_dlq12mth', 'no_time_dlqlife', 'note_rate_range', 'occupancy_range', 'property_subtype', 'special_servicer', 'state']
+
+    # if plot_hists == 'y':
+    #     plot_histograms(df_mspd, hist_columns, name = 'Histogram of Freddie Columns')
